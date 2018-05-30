e6449fafa6ec7fd0d5a814fd6ee6f1850b355d9d
Fede <fehc97@hotmail.com>
2018-05-30 17:55:27 -0300

    Agrego clase regla

--- src/main/java/proyectoDDSs/Regla.java
+++ src/main/java/proyectoDDSs/Regla.java
@@ -1,13 +1,25 @@
 package proyectoDDSs;
 
+import proyectoDDSs.Actuador;
+
+import java.util.*;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Regla {
-
-	public void actualizarMedicion(double valor) {
-		
-		
-		
-	}
-
 	
+	double condicionDeAccion;
+	protected ArrayList<Actuador> ActuadoresAAccionar = new ArrayList<Actuador>();
+	
+	public void actualizarMedicion(double medicion){
+		if (medicion<condicionDeAccion){
+			this.accionarActuadores();
+		}
+	}
+	
+	public void accionarActuadores() {
+		ActuadoresAAccionar.stream().map(actuador->actuador.execute());
+	}
 	
 }
+
