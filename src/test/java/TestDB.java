b7cee99b354719beacc73f62da1359c6bbcd54bd
lucasresa <lukkitas.1597@gmail.com>
2018-10-09 11:39:57 -0300

    Agregue un par de dependencias al pom y modifique el .xml de la db para probarla, y estuve haciendo pruebas con el ModelHelper que nos mandaron x mail para ayudarnos con el tema de persistir cosas a la base

---
+++ src/test/java/TestDB.java
@@ -0,0 +1,13 @@
+import models.ModelHelper;
+
+import javax.persistence.*;
+
+public class TestDB {
+
+	public static void main(String[] args) {
+		
+		EntityManager e = ModelHelper.getEntityManager();
+					
+	}
+
+}
