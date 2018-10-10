4f4df70e067d51842d7e1df82a54bfa05d700809
Dazztian <diegodazpeitia@gmail.com>
2018-10-09 21:28:41 -0300

    Agregados los models de las clases a persistir

--- src/main/java/modelsPersistencia/DispositivoModel.java
+++ src/main/java/modelsPersistencia/DispositivoModel.java
@@ -1,5 +1,22 @@
 package modelsPersistencia;
+import java.util.List;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
-public class DispositivoModel {
+import proyectoDDSs.Dispositivo;
 
+
+	public class DispositivoModel extends ModelHelperPersistencia{
+
+		public DispositivoModel() {
+			super();
+		}
+		
+		public Dispositivo buscarDispositivo(int id) {
+			return super.buscar(Dispositivo.class, new ImmutablePair<>("id", id));
+		}
+		
+		public List<Dispositivo> buscarTodosLosDispositivo(){
+			return super.buscarTodos(Dispositivo.class);
+		}
 }
+ 
