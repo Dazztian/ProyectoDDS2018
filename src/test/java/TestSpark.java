b0a11e6ef09e3f3d56cfbca3c29f6f641456da52
Dazztian <diegodazpeitia@gmail.com>
2018-11-04 23:03:55 -0300

    otra version de prueba de spark

--- src/test/java/TestSpark.java
+++ src/test/java/TestSpark.java
@@ -1,11 +1,7 @@
-import org.apache.log4j.Logger;
-import org.apache.log4j.BasicConfigurator;
- 
+import static spark.Spark.*;
+
 public class TestSpark {
-	static Logger log = Logger.getLogger(TestSpark.class);
- 
-	public static void main(String[] args) {
-		BasicConfigurator.configure();
-		log.info("This is Logger Info");
-	}
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
 }
