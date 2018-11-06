c985b3562ddbbec512b05841c80f8266b1659833
Fede <fehc97@hotmail.com>
2018-11-06 12:18:09 -0300

    subo metodos de reportes del admin desde java sin testear

--- src/main/java/proyectoDDSs/Administrador.java
+++ src/main/java/proyectoDDSs/Administrador.java
@@ -6,7 +6,10 @@ import java.util.*;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.Table;
+
+import modelsPersistencia.ModelHelperPersistencia;
 
 @Entity
 @DiscriminatorValue("A")
@@ -36,7 +39,7 @@ public class Administrador extends Usuario {
 			LocalDateTime fechaActual=LocalDateTime.now();
 			Duration duration = Duration.between(fechaActual, fechaCreacion);
 		    //Diferencia en cantDeDias -- Lo divido por 30 para que tire los meses
-			return (long)Math.abs(duration.toDays()/30); //Hago un casteo para que no me de nº con coma
+			return (long)Math.abs(duration.toDays()/30); //Hago un casteo para que no me de nï¿½ con coma
 		   
 		}
 		
@@ -46,5 +49,47 @@ public class Administrador extends Usuario {
 			return (long)Math.abs(duracion.toDays()/30);
 		}
 		
-	
+		public double generarReportHogarXPeriodo(Cliente cliente, LocalDateTime fechaInicial, LocalDateTime fechaFinal) {
+
+			double resultado=0;
+			ModelHelperPersistencia mh = new ModelHelperPersistencia();
+			EntityManager em = mh.entityManager();
+			
+			List<Object[]> resultadoQuery = 
+					em.createNativeQuery("select sum(consumo) suma " +
+									"from usuarios u " +
+										"join dispositivo_cliente dc on (u.id = dc.id_cliente) " +
+										"join consumos c on (c.id_dispositivo = dc.Id_DispoCliente) " +	
+									"where u.id = " + cliente.getId() + "And c.fecha between " + fechaInicial + " and " + fechaFinal + 
+									" group by u.id"
+									).getResultList();
+			
+			for(Object[] resultadoSingular : resultadoQuery) {
+				resultado = (double) resultadoSingular[0];
+			}
+			return resultado;
+		}
+		
+		public double generarReportePromedioXPeriodo(LocalDateTime fechaInicial, LocalDateTime fechaFinal) {
+			
+			double resultado=0;
+			ModelHelperPersistencia mh = new ModelHelperPersistencia();
+			EntityManager em = mh.entityManager();
+			
+			List<Object[]> resultadoQuery = 
+					em.createNativeQuery("select dc.Tipo_Dispositivo, avg(consumo) PromedioDeConsumo\r\n" + 
+							"	from dispositivo_cliente dc \r\n" + 
+							"		join consumos c on (dc.Id_DispoCliente = c.id_dispositivo)\r\n" + 
+							"    where c.fecha between " + fechaInicial + 
+							" and " + fechaFinal + 
+							"    group by dc.Tipo_Dispositivo").getResultList();
+			
+			for(Object[] resultadoSingular : resultadoQuery) {
+				resultado = (double) resultadoSingular[0];
+			}
+			return resultado;
+		}
 }
+				
+	
+
