69a4632b2b7f5899363ca6561c5a0fe3f8ca1772
Fede <fehc97@hotmail.com>
2018-11-26 19:48:53 -0300

    saco tests

--- src/main/java/spark/server/Server.java
+++ src/main/java/spark/server/Server.java
@@ -17,6 +17,7 @@ public class Server {
 	public static void main(String[] args) {
 		
 		Spark.port(9000);
+		//Spark.port(getHerokuAssignedPort());
 	
 		HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
 		EntityManager em = ModelHelperPersistencia.getEntityManager();
@@ -83,5 +84,13 @@ public class Server {
 		DebugScreen.enableDebugScreen();
 		
 	}
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 
 }
--- src/test/java/TestSpark.java
+++
@@ -1,7 +0,0 @@
-import static spark.Spark.*;
-
-public class TestSpark {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
-}
